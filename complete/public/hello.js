3872c44be7ecd429f92622b3ef66dc6ecc6267b3
oksanab91 <oksanab767@gmail.com>
2018-05-20 22:12:45 -0700

    Change API call port.

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -1,6 +1,8 @@
+//http://rest-service.guides.spring.io/greeting
+//open on : http://localhost:8080/
 angular.module('demo', [])
 .controller('Hello', function($scope, $http) {
-    $http.get('http://rest-service.guides.spring.io/greeting').
+    $http.get('http://localhost:8181/greeting?name=User').
         then(function(response) {
             $scope.greeting = response.data;
         });
