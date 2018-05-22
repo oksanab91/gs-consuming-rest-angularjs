006a68571557c0a74a4c46dab9eb6047659242b1
oksanab91 <oksanab767@gmail.com>
2018-05-21 17:49:52 -0700

    Add call for endpoint with PathVariable.

--- complete/public/hello.js
+++ complete/public/hello.js
@@ -6,4 +6,14 @@ angular.module('demo', [])
         then(function(response) {
             $scope.greeting = response.data;
         });
+
+    // $http.get('http://localhost:8181/{name}/greeting').
+    // then(function(response) {
+    //     $scope.greeting = response.data;
+    // });
+}).controller('Rita', function($scope, $http) {
+    $http.get('http://localhost:8181/ritulka/greeting').
+        then(function(response) {
+            $scope.greeting = response.data;
+        });
 });
